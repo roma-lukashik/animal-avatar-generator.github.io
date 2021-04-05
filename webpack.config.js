44c6620c5b6465570abd3fe3b126e85a451376f8
romanlukashik <roman.lukashik@gmail.com>
2021-04-05 20:19:39 +0300

    Change webpack output path

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
   mode: 'development',
   entry: './index.ts',
+  output: {
+    path: path.resolve(__dirname, 'animal-avatar-generator'),
+  },
   resolve: {
     extensions: ['.js', '.ts'],
   },
