cd171bd2dda92c75fd3399ee3e7507d433bb36fd
romanlukashik <roman.lukashik@gmail.com>
2021-04-06 13:23:42 +0300

    Add build script

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,43 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
-module.exports = {
-  mode: 'development',
-  entry: './index.ts',
-  output: {
-    path: path.resolve(__dirname, 'animal-avatar-generator'),
-  },
-  resolve: {
-    extensions: ['.js', '.ts'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-typescript',
-            ],
-            plugins: [
-              ['@babel/plugin-transform-runtime'],
-            ],
+module.exports = (env, argv) => {
+  return {
+    mode: argv.mode || 'none',
+    entry: './index.ts',
+    output: {
+      path: path.resolve(__dirname, 'animal-avatar-generator'),
+    },
+    resolve: {
+      extensions: ['.js', '.ts'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-typescript',
+              ],
+              plugins: [
+                ['@babel/plugin-transform-runtime'],
+              ],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
     ],
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-  ],
+  }
 }
